2d88c622ba1dbadfc72691ac57c6ccedcd865c59
Niud <nitrolasbusle@gmail.com>
2026-09-10 21:42:03 -0500

    Create PoblarTablaColaboradores.sql

---
+++ PoblarTablaColaboradores.sql
@@ -0,0 +1,30 @@
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1010234567, 'Andres Felipe Morales', 'PLANTA', 12);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion)            VALUES ('CC', 1020345678, 'Laura Camila Rojas', 'PLANTA');
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1030456789, 'Juan Sebastian Torres', 'PLANTA', 15);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1040567890, 'Diana Marcela Pineda', 'TEMPORAL', 8);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion)            VALUES ('CC', 1050678901, 'Carlos Andres Beltran', 'TEMPORAL');
+
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1060789012, 'Maria Fernanda Quintero', 'PLANTA', 10);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1070890123, 'Julian David Castano', 'TEMPORAL', 7);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1080901234, 'Angela Patricia Suarez', 'PLANTA', 14);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CE', 1080901234, 'Mateo Alejandro Prieto', 'TEMPORAL', 9);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion)            VALUES ('CC', 1090123456, 'Valentina Ospina Ruiz', 'PLANTA');
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1100234567, 'Santiago Herrera Lopez', 'TEMPORAL', 11);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1110345678, 'Paula Andrea Cardenas', 'PLANTA', 13);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1120456789, 'Nicolas Esteban Vargas', 'TEMPORAL', 6);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion)            VALUES ('CC', 1130567890, 'Daniela Sofia Mendez', 'PLANTA');
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1140678901, 'Felipe Antonio Guzman', 'TEMPORAL', 10);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('TI', 1150789012, 'Camila Lucia Espinosa', 'TEMPORAL', 5);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1160890123, 'Sergio Ivan Naranjo', 'PLANTA', 16);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion)            VALUES ('CC', 1170901234, 'Natalia Gomez Chaves', 'PLANTA');
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1180012345, 'Oscar Mauricio Rincon', 'TEMPORAL', 8);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1190123456, 'Isabella Duarte Pena', 'PLANTA', 12);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CE', 1200234567, 'Tomas Eduardo Salazar', 'TEMPORAL', 9);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion)            VALUES ('CC', 1210345678, 'Juliana Restrepo Arias', 'PLANTA');
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1220456789, 'Emanuel Rivera Cortes', 'TEMPORAL', 10);
+
+
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1230567890, 'Sofia Alejandra Nieto', 'PLANTA', 10);
+INSERT INTO colaborador (tipodocumento, numerodocumento, nombre, vinculacion, comision) VALUES ('CC', 1240678901, 'Ricardo Esteban Gomez', 'TEMPORAL', 10);
+
+COMMIT;
